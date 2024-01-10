6694e07031312855f9ab3b4e74be76ceadb26d92
Julia Mezenova <julia21moon86@mail.ru>
2024-01-10 18:07:19 +0500

    modified page_analyzer/app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 import os
 
 
+load_dotenv()  # take environment variables from .env.
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-
-load_dotenv()  # take environment variables from .env.
 
 
 @app.route("/")
