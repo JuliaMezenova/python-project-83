7968fb8ac25f9fd7067df4dceba08af1c600b476
Julia Mezenova <julia21moon86@mail.ru>
2024-01-10 13:00:29 +0500

    linter errors

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from dotenv inport load_dotenv
+from dotenv import load_dotenv
+import os
 
 
 app = Flask(__name__)
@@ -7,6 +8,7 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 load_dotenv()  # take environment variables from .env.
 
+
 @app.route("/")
 def hello_world():
-        return "<p>Hello, World!</p>"
+    return "<p>Hello, World!</p>"
