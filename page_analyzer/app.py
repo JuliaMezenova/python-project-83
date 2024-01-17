36af5b5b4b4ae2988ed0eb7ba6052e989b99443f
Julia Mezenova <julia21moon86@mail.ru>
2024-01-17 12:55:25 +0500

    modified page_analyzer/app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,12 +1,14 @@
 from flask import (
-        Flask,
-        render_template,
-        request,
-        flash,
-        get_flashed_messages,
-        url_for,
-        redirect
-        )
+    Flask,
+    render_template,
+    request,
+    flash,
+    get_flashed_messages,
+    url_for,
+    redirect
+)
+import psycopg2
+import psycopg2.extras
 from dotenv import load_dotenv
 import os
 from .validator import validate
@@ -26,28 +28,35 @@ def index():
     messages = get_flashed_messages(with_categories=True)
     return render_template('index.html', messages=messages)
 
+
 @app.post("/urls")
 def add_url():
     url = request.form.get('url')
     normalized_url = normalize(url)
-    errors = validate(normalized_url):
+    errors = validate(normalized_url)
     if errors:
         flash(errors, 'errors')
-        messages = get_flashed_messages(with_categories=True)    
+        messages = get_flashed_messages(with_categories=True)
         return render_template(
-                'index.html',
-                input_url=url
-                messages=messages
-                ), 422
+            'index.html',
+            input_url=url,
+            messages=messages
+        ), 422
     with conn:
         with conn.cursor as curs:
-            curs.execute("SELECT * FROM urls WHERE name = %s;", (normalized_url,))
+            curs.execute(
+                "SELECT * FROM urls WHERE name = %s;",
+                (normalized_url,)
+            )
             result_url = curs.fetchone()
             if result_url:
                 flash("Cтраница уже существует", 'info')
                 id = result_url.id
             else:
-                curs.execute("INSERT INTO urls (name) VALUES (%s);", (normalized_url,))
+                curs.execute(
+                    "INSERT INTO urls (name) VALUES (%s);",
+                    (normalized_url,)
+                )
                 conn.commit()
                 id = curs.fetchone()[0]
                 curs.execute("SELECT * FROM urls WHERE id = %s;", (id,))
@@ -56,6 +65,7 @@ def add_url():
         curs.close()
     conn.close()
     return redirect(url_for('show_url', id=id))
+
 
 @app.get("/urls/<int:id>")
 def show_url(id):
@@ -70,7 +80,7 @@ def show_url(id):
         "show_url.html",
         url=result_url,
         messages=messages
-        )
+    )
 
 
 @app.get("/urls")
@@ -81,7 +91,7 @@ def show_urls():
             result_urls = curs.fetchall()
         curs.close()
     conn.close()
-    return render_template(    
+    return render_template(
         "show_all_urls.html",
         urls=result_urls
-        )
+    )
